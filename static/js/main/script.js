be659d2e36fbf8dbc2185a4aeb8878a3294774d1
Ben Wang <ben.wang93@gmail.com>
2014-12-07 15:13:11 -0500

    added soundcloud api support

--- static/js/main/script.js
+++ static/js/main/script.js
@@ -1,18 +1,37 @@
+// Replace all API Wrappers from SoundCloud .live to .on
+
 // initialize client with app credentials
 SC.initialize({
   client_id: 'e70a2323a96fbe32617ccb56d8df6d85',
-  redirect_uri: 'localhost/callback.html',
+
+  // change this uri to actual hosted callback.html
+  redirect_uri: 'http://run2walkaway.github.io/callback.html',
 });
 
 // initiate auth popup
+/*
 SC.connect(function() {
   SC.get('/me', function(me) {
     alert('Hello, ' + me.username);
   });
 });
+*/
 
-$("#stream").live("click", function(){
-  SC.stream("/tracks/293", {autoPlay: true});
+// Gives top track widget
+/*
+$("#loadTracks").on("click", function(){
+  SC.get("/tracks", {limit: 1}, function(tracks){
+    var track = tracks[0];
+    SC.oEmbed(track.uri, document.getElementById("track"));
+  });
 });
+*/
+
+// Gives a specific track widget
+/*
+$("#stream").on("click", function(){
+  $SC.stream("/tracks/293", {autoPlay: true});
+});
+*/
 
 // SC.oEmbed("http://soundcloud.com/forss/sets/soulhack", {color: "ff0066"}, document.getElementById("player"));

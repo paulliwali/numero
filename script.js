e90294d6de38c842b8b0fd704ae559a217971199
Amelia Ly <amelia.ly39@gmail.com>
2014-12-07 11:25:47 -0500

    testing soundcloud api

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// initialize client with app credentials
 SC.initialize({
   client_id: 'YOUR_CLIENT_ID'
+  redirect_uri: 'file://localhost/Users/Milo/numero/callback.html'
 })
 
+// initiate auth popup
+SC.connect(function() {
+  SC.get('/me', function(me) { 
+    alert('Hello, ' + me.username); 
+  });
+});
+
 $(document).read(function() {
-  SC.get('/tracks', { genres: 'pop' }, function(tracks) {
-    $(tracks).each(function(index, track) {
-      $('#results').append($('<li></li>').html(track.title + ' - ' +_track.genre));
+	SC.get('/tracks/293', function(track) {
+		SC.oEmbed(track.permalink_url, document.getElementById('player'));
     });
   });
 });
